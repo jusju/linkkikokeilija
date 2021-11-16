ddf5d7f1cc3b748236957b2fb085c9a79bcfae77
Juslin Jukka <jusju@haaga-helia.fi>
2021-11-16 11:47:37 +0200

    first commit

--- src/moottori/BrokenLinks.java
+++ src/moottori/BrokenLinks.java
@@ -1,81 +1,58 @@
 package moottori;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Iterator;
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
 
 public class BrokenLinks {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","./chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://ohjelmointi2.github.io/");
 
-	private static WebDriver driver = null;
+        //Storing the links in a list and traversing through the links
+        List<WebElement> links = driver.findElements(By.tagName("a"));
 
-	public static void main(String[] args) {
-// TODO Auto-generated method stub
+        // This line will print the number of links and the count of links.
+        System.out.println("No of links are "+ links.size());  
+      
+        //checking the links fetched.
+        for(int i=0;i<links.size();i++)
+        {
+            WebElement E1= links.get(i);
+            String url= E1.getAttribute("href");
+            verifyLinks(url);
+        }
+        
+        driver.quit();
+    }
+    
+    
+    public static void verifyLinks(String linkUrl)
+    {
+        try
+        {
+            URL url = new URL(linkUrl);
 
-		String homePage = "https://ohjelmointi2.github.io/";
-		String url = "";
-		HttpURLConnection huc = null;
-		int respCode = 200;
-
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		driver = new ChromeDriver();
-		
-		driver.manage().window().maximize();
-
-		driver.get(homePage);
-
-		List<WebElement> links = driver.findElements(By.tagName("a"));
-
-		Iterator<WebElement> it = links.iterator();
-
-		while (it.hasNext()) {
-
-			url = it.next().getAttribute("href");
-
-			System.out.println(url);
-
-			if (url == null || url.isEmpty()) {
-				System.out.println("URL is either not configured for anchor tag or it is empty");
-				continue;
-			}
-
-			if (!url.startsWith(homePage)) {
-				System.out.println("URL belongs to another domain, skipping it.");
-				continue;
-			}
-
-			try {
-				huc = (HttpURLConnection) (new URL(url).openConnection());
-
-				huc.setRequestMethod("HEAD");
-
-				huc.connect();
-
-				respCode = huc.getResponseCode();
-
-				if (respCode >= 400) {
-					System.out.println(url + " is a broken link");
-				} else {
-					System.out.println(url + " is a valid link");
-				}
-
-			} catch (MalformedURLException e) {
-// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
-		driver.quit();
-
-	}
+            //Now we will be creating url connection and getting the response code
+            HttpURLConnection httpURLConnect=(HttpURLConnection)url.openConnection();
+            httpURLConnect.setConnectTimeout(5000);
+            httpURLConnect.connect();
+            if(httpURLConnect.getResponseCode()>=400)
+            {
+            	System.out.println(linkUrl+" - "+httpURLConnect.getResponseMessage()+"is a broken link");
+            }    
+       
+            //Fetching and Printing the response code obtained
+            else{
+                System.out.println(linkUrl+" - "+httpURLConnect.getResponseMessage());
+            }
+        }catch (Exception e) {
+      }
+   }
 }
